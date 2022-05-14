74fd4c8f0118e90f90a78487ca42dee992e8b9c0
pegah <100560899+pegah1378@users.noreply.github.com>
2022-05-14 22:18:32 +0430

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center" id="App-footer">
+          <small>coded by Pegah</small>
+        </footer>
+      </div>
     </div>
   );
 }
