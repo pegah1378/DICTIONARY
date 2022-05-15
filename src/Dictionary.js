ee68a1b23ff6e48c286e9f4d92168d9154532ff9
pegah <100560899+pegah1378@users.noreply.github.com>
2022-05-15 15:54:43 +0430

    Meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -20,6 +23,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
