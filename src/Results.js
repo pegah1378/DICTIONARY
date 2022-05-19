c6aa97221c9bb4fc5f52ce934663b9bea20dccb5
pegah <100560899+pegah1378@users.noreply.github.com>
2022-05-19 21:52:07 +0430

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -6,10 +6,10 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetics, index) {
+        {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
-              <Phonetic phonetic={Phonetic} />
+              <Phonetic phonetic={phonetic} />
             </div>
           );
         })}
